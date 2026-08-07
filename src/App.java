06e90c2009e9b115afe4a1856304592ffc676591
Braylon Cassidy <cassibra000@warren.k12.in.us>
2026-08-07 12:53:48 -0400

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,24 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String myName = "Braylon";
+        System.out.println(myName);
+
+        myName = "Cassidy";
+        System.out.println(myName);
+
+        char myLetter = '\u00A9';
+        System.out.println(myLetter);
+
+        int x = 3;
+        short y = 3;
+        long z = 36287427284903L;
+        byte b = 3;
+
+        double d = 3.14112435607798543;
+        float f = 3.14112435607798543F;
+
+        boolean myBool = true;
+        boolean myBool2 = false;
+    }
+}
